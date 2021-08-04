d5470b0ca326b585f9c2ee8fa6b3b56a5e72bb5c
jongsik <kjsik11@gmail.com>
2021-08-04 16:39:45 +0900

    fix codes

--- src/routes/v1/trade.js
+++ src/routes/v1/trade.js
@@ -1,15 +1,13 @@
 import { Router } from 'express';
 import Joi from 'joi';
-import request from 'request';
+import got from 'got';
 import { parseStringPromise } from 'xml2js';
+import httpStatus from 'http-status';
 
 import checkApiKey from '../../middlewares/check-api-key.js';
+import ApiError from '../../utils/api-error.js';
 
 const router = Router();
-
-const checkBodySchema = Joi.object({
-  applicationNumber: Joi.number().label('applicationNumber').required(),
-});
 
 const accessKey = process.env.KIPRIS_ACCESS_KEY;
 const adminKey = process.env.ADMIN_KEY;
@@ -29,44 +27,53 @@ router.get('/', async (req, res) => {
   return res.json({ tradeMarkes: data.map((val) => val.tradeMarkInfo) });
 });
 
-router.post('/', async (req, res) => {
+router.post('/', async (req, res, next) => {
+  const checkBodySchema = Joi.object({
+    applicationNumber: Joi.number().label('applicationNumber').required(),
+  });
+
   const { applicationNumber } = await checkBodySchema
     .validateAsync(req.body)
-    .catch((err) => res.status(400).json({ error: err.message }));
+    .catch((err) => next(new ApiError(httpStatus.BAD_REQUEST, err.message)));
 
-  // FIXME: 🤮 lol 🤮
-  await request.get({ url, qs: { applicationNumber, accessKey } }, async (err, response) => {
-    if (err) res.status(404).json({ error: 'trademark server error' });
+  try {
+    const xmlData = await got
+      .get(url, {
+        searchParams: {
+          accessKey,
+          applicationNumber,
+        },
+      })
+      .text();
 
     const {
       response: {
         body: { items },
       },
-    } = await parseStringPromise(response.body, {
+    } = await parseStringPromise(xmlData, {
       explicitArray: false,
     });
 
-    if (items.TotalSearchCount === '0') {
-      res.status(404).send({ result: 'No such trademark item' });
-    } else {
-      const { db } = req.app.locals;
+    if (items.TotalSearchCount === '0')
+      return next(new ApiError(httpStatus.NOT_FOUND, 'No such trademark item'));
 
-      const tradeMark = await db
-        .collection('trademark')
-        .findOne({ 'tradeMarkInfo.ApplicationNumber': String(applicationNumber), deleted: null });
+    const { db } = req.app.locals;
 
-      if (tradeMark) {
-        res.status(304).json({ result: 'already exist applicationId' });
-      } else {
-        const { insertedId } = await db.collection('trademark').insertOne({
-          tradeMarkInfo: items.TradeMarkInfo,
-          createdAt: new Date(),
-        });
+    const tradeMark = await db
+      .collection('trademark')
+      .findOne({ 'tradeMarkInfo.ApplicationNumber': String(applicationNumber), deleted: null });
 
-        res.json({ insertedId });
-      }
-    }
-  });
+    if (tradeMark) return next(new ApiError(httpStatus.NOT_MODIFIED));
+
+    const { insertedId } = await db.collection('trademark').insertOne({
+      tradeMarkInfo: items.TradeMarkInfo,
+      createdAt: new Date(),
+    });
+
+    return res.json({ insertedId });
+  } catch (err) {
+    return next(new ApiError(res.status, err.message));
+  }
 });
 
 export default router;
